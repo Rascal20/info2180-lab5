081aef4ba960875c8c5d9bb8a0f8deec3468a6e5
Rene Tim <rdatim20@gmail.com>
2021-11-09 09:43:26 -0400

    Outputing data as a table instead of a list

--- world.php
+++ world.php
@@ -10,8 +10,34 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
 $requested_country = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $requested_country->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-<?php foreach ($results as $row): ?>
+
+<?php if(isset($country)): ?>
+
+  <table class = "showTable">
+    <caption><h2> COUNTRIES </h2></caption>
+    <thead>
+      <tr>
+        <th class = "t1"> COUNTRY NAME </th>
+        <th id = "t2"> CONTINENT </th>
+        <th id = "t3"> INDEPENDENCE </th>
+        <th id = "t4"> HEAD OF STATE </th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($results as $country): ?>
+        <tr>
+          <td><?php echo $country["name"]; ?></td>
+          <td><?php echo $country["continent"]; ?></td>
+          <td><?php echo $country["independence_year"]; ?></td>
+          <td><?php echo $country["head_of_state"]; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php endif; ?>
+
+
+<?php /*foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' .  $row['head_of_state']; ?></li>
-<?php endforeach;?>
+<?php endforeach;?>*/
