932292f1f89630c70a81e5a809070d5a2037e106
Rene Tim <rdatim20@gmail.com>
2021-11-09 07:45:33 -0400

    Allowing search by country from database

--- world.php
+++ world.php
@@ -1,17 +1,17 @@
 <?php
+
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+$requested_country = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$results = $requested_country->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+  <li><?= $row['name'] . ' is ruled by ' .  $row['head_of_state']; ?></li>
+<?php endforeach;?>
